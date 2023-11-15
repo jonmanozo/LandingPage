54b9aa4fbc983f29d6b0b3f49f13cd6d2a664620
jonmanozo <138876688+jonmanozo@users.noreply.github.com>
2023-11-15 18:49:44 +0800

    Update Root.jsx

--- src/Root.jsx
+++ src/Root.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import YowIam from './components/YowIam'
 import CanDo from './components/CanDo'
-import Projects from './components/projects'
-import Footer from './components/footer'
+import Projects from './components/Projects'
+import Footer from './components/Footer'
 
 export default function Root() {
   return (
