57070085223586a32068ce9dc1900bd440a386e5
jonmanozo <138876688+jonmanozo@users.noreply.github.com>
2023-11-20 23:23:40 +0800

    Change title,

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <>
       <div className="fixed flex justify-center items-center w-[100%] py-2 bg-black z-20">
             <div className="flex w-[80%] text-white ">
-                <div className="w-[50%] flex items-center text-[30px] font-bold"><h1  className='text-red'>ALJON MAÑOZO</h1></div>
+                <div className="w-[50%] flex items-center text-[30px] font-bold justify-center "><h1  className='text-red'>ALJON MAÑOZO</h1></div>
                 <div className="w-[10%] flex items-center justify-center font-bold "><a href="#Iam" className='NavLinkhover'>WHO AM I</a></div>
                 <div className="w-[10%] flex items-center justify-center font-bold "><a href="#CanDo"  className='NavLinkhover'>CAN DO</a></div>
                 <div className="w-[10%] flex items-center justify-center font-bold "><a href="#Projects"  className='NavLinkhover'>PROJECTS</a></div>
--- src/components/YowIam.jsx
+++ src/components/YowIam.jsx
@@ -10,8 +10,8 @@ export default function YowIam() {
             <div className="flex w-[45%]"><img src="/images/intro.png" alt="Image" /></div>
             <div className="flex justify-center items-center w-[45%] text-white">
                 <div class="flex flex-col w-[100%]">
-                    <div class="text-[80px] font-bold">YOW! I AM</div>
-                    <div class="text-[90px] font-bold">ALJON</div>
+                    <div class="text-[80px] font-bold"><span>YOW!</span></div>
+                    <div class="text-[90px] font-bold">I AM  ALJON</div>
                     <div class="text-[60px] font-bold text-gray-500"><strong className='text-red text-[80px] animate-pulse -z-2'>FULL STACK</strong> <br />WEB DEVELOPER</div>
                 </div>
             </div>
