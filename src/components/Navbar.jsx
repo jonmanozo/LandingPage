b773db1ce81656186ebf136109a78dd6d5a3a206
jonmanozo <138876688+jonmanozo@users.noreply.github.com>
2023-11-26 00:27:16 +0800

    Adding new animation
    project rotate while in view
    project text animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,8 @@ export default function Navbar() {
             <div className="flex w-[80%] text-white ">
                 <div className="w-[50%] flex items-center text-[30px] font-bold justify-center "><h1  className='text-red'>ALJON MAÑOZO</h1></div>
                 <div className="w-[10%] flex items-center justify-center font-bold "><a href="#Iam" className='NavLinkhover'>WHO AM I</a></div>
-                <div className="w-[10%] flex items-center justify-center font-bold "><a href="#CanDo"  className='NavLinkhover'>CAN DO</a></div>
                 <div className="w-[10%] flex items-center justify-center font-bold "><a href="#Projects"  className='NavLinkhover'>PROJECTS</a></div>
+                <div className="w-[10%] flex items-center justify-center font-bold "><a href="#CanDo"  className='NavLinkhover'>CAN DO</a></div>
                 <div className="w-[10%] flex items-center justify-center font-bold "><a href="#Contact"  className='NavLinkhover'>CONTACT</a></div>
             </div>
       </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,12 +11,30 @@ const textVariant = {
     x: 0,
     opacity: 1,
     transition:{
-      duration:1.5,
+      duration:1,
+      staggerChildren: 0.3,
+    },
+  },
+
+};
+
+
+const textVariant2 = {
+  initial: {
+    x:-500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition:{
+      duration:1,
       staggerChildren: 0.1,
     },
   },
 
 };
+
 
 
 
@@ -31,21 +49,25 @@ export default function Projects() {
                 <motion.div 
                   className="flex w-[40%] h-[500px] rounded-xl bg-gray-500"
                   initial={{opacity: 0, scale: 0.5}}
-                  whileInView={{opacity: 1, scale: 1, x:10, y:10}}
-                  whileHover={{rotate: -7}}
-                  transition={{duration: 0.5}}
+                  whileInView={{opacity: 1, scale: 1, x:10, y:10, rotate: 360}}
+                  whileHover={{rotate: 367}}
+                  transition={{duration: 0.8}}
                 >
 
                 </motion.div>
 
                 <div className="flex w-[50%] h-[500px]">
-                    <div className="flex flex-col justify-center w-100 text-gray-300 pl-10">
-                        <span className='text-[60px] font-bold'>EASYSHOP</span>
-                        <p className='mt-8 text-[20px]'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum accusantium commodi 
+                <motion.div className="flex flex-col justify-center w-100 text-gray-300 pl-10"
+                      variants={textVariant}
+                      initial="initial"
+                      whileInView="animate"
+                    >
+                        <motion.div className='text-[60px] font-bold'  variants={textVariant}>EASYSHOP</motion.div>
+                        <motion.p className='mt-8 text-[20px]'  variants={textVariant}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum accusantium commodi 
                           quod quasi fuga aliquam. Veniam eveniet quam sed, dolor error, voluptate non rem natus, 
-                          repellat dolores fuga accusamus impedit!</p>
-                        <button className='w-[200px] py-2 mt-8 font-bold rounded-lg bg-red'>See Demo</button>
-                    </div>
+                          repellat dolores fuga accusamus impedit!</motion.p>
+                        <motion.button className='w-[200px] py-2 mt-8 font-bold rounded-lg bg-red'  variants={textVariant}>See Demo</motion.button>
+                    </motion.div>
                 </div>
             </div>
 
@@ -59,25 +81,27 @@ export default function Projects() {
             <div className="flex justify-evenly items-center w-[80%]">
               
             <div className="flex w-[50%] h-[500px]">
-                    <motion.div className="flex flex-col justify-center w-100 text-gray-300 pl-10"
-                      variants={textVariant}
+                    <motion.div className="flex flex-col justify-center items-end w-100 text-gray-300 pl-10"
+                      variants={textVariant2}
                       initial="initial"
-                      animate="animate"
+                      whileInView="animate"
                     >
-                        <motion.div className='text-[60px] font-bold'  variants={textVariant}>EASYSHOP</motion.div>
-                        <motion.p className='mt-8 text-[20px]'  variants={textVariant}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum accusantium commodi 
+                        <motion.div className='text-[60px] font-bold'  variants={textVariant2}>EASYSHOP</motion.div>
+                        <motion.p className='mt-8 text-[20px] text-right'  variants={textVariant2}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum accusantium commodi 
                           quod quasi fuga aliquam. Veniam eveniet quam sed, dolor error, voluptate non rem natus, 
                           repellat dolores fuga accusamus impedit!</motion.p>
-                        <motion.button className='w-[200px] py-2 mt-8 font-bold rounded-lg bg-red'  variants={textVariant}>See Demo</motion.button>
+                        <motion.button className='w-[200px] py-2 mt-8 font-bold rounded-lg bg-red'  variants={textVariant2}>See Demo</motion.button>
                     </motion.div>
                 </div>
+
                 <motion.div 
                   className="flex w-[40%] h-[500px] rounded-xl bg-gray-500"
                   initial={{opacity: 0, scale: 0.5}}
-                  whileInView={{opacity: 1, scale: 1, x:10, y:10}}
-                  whileHover={{rotate: 7}}
-                  transition={{duration: 0.5}}
+                  whileInView={{opacity: 1, scale: 1, x:10, y:10, rotate: 360}}
+                  whileHover={{rotate: 367}}
+                  transition={{duration: 0.8}}
                 >
+
 
                 </motion.div>
 
@@ -91,24 +115,28 @@ export default function Projects() {
         <div id="Projects" className="flex justify-center w-[100%] h-[100vh] bg-black">
 
             <div className="flex justify-evenly items-center w-[80%]">
-                <motion.div 
+            <motion.div 
                   className="flex w-[40%] h-[500px] rounded-xl bg-gray-500"
                   initial={{opacity: 0, scale: 0.5}}
-                  whileInView={{opacity: 1, scale: 1, x:10, y:10}}
-                  whileHover={{rotate: -7}}
-                  transition={{duration: 0.5}}
+                  whileInView={{opacity: 1, scale: 1, x:10, y:10, rotate: 360}}
+                  whileHover={{rotate: 367}}
+                  transition={{duration: 0.8}}
                 >
 
                 </motion.div>
 
                 <div className="flex w-[50%] h-[500px]">
-                    <div className="flex flex-col justify-center w-100 text-gray-300 pl-10">
-                        <span className='text-[60px] font-bold'>EASYSHOP</span>
-                        <p className='mt-8 text-[20px]'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum accusantium commodi 
+                <motion.div className="flex flex-col justify-center w-100 text-gray-300 pl-10"
+                      variants={textVariant}
+                      initial="initial"
+                      whileInView="animate"
+                    >
+                        <motion.div className='text-[60px] font-bold'  variants={textVariant}>EASYSHOP</motion.div>
+                        <motion.p className='mt-8 text-[20px]'  variants={textVariant}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum accusantium commodi 
                           quod quasi fuga aliquam. Veniam eveniet quam sed, dolor error, voluptate non rem natus, 
-                          repellat dolores fuga accusamus impedit!</p>
-                        <button className='w-[200px] py-2 mt-8 font-bold rounded-lg bg-red'>See Demo</button>
-                    </div>
+                          repellat dolores fuga accusamus impedit!</motion.p>
+                        <motion.button className='w-[200px] py-2 mt-8 font-bold rounded-lg bg-red'  variants={textVariant}>See Demo</motion.button>
+                    </motion.div>
                 </div>
             </div>
 
