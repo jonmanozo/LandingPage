65416ee6e75c462de121c1177c53a78083511a0a
jonmanozo <jonmanozo@gmail.com>
2023-11-29 17:22:55 +0800

    CAN DO AND FOOTER
    UNFINISHED

--- src/components/CanDo.jsx
+++ src/components/CanDo.jsx
@@ -3,82 +3,54 @@ import React from 'react'
 export default function CanDo() {
   return (
     <>
-    <section>
-        <div id="CanDo" className="flex justify-center w-[100%] h-[100vh] bg-darkgray">
-          <div class="flex flex-col w-[55%] ">
-              <h2 className='text-red font-bold text-[40px] mt-10'>I CAN</h2>
-              <div class="w-[100%] h-[750px] overflow-y-auto pr-10 pl-10">
-                    <div class="flex  bg-backgray p-5 mt-5  hover-rotate"> 
-                        
-                        <div class="flex justify-center items-center w-[25%] h-[100%]">
-                          <img src="/images/DevLogo.png" alt="" className='w-[50%] h-[50%]'/>
+    <section className=' bg-darkgray'>
+        <div id="CanDo" className="flex justify-center flex-col w-[100%] ">
+          
+        <h2 className='flex w-[100%] justify-center text-red font-bold text-[40px] mt-10 mb-10'>I CAN DO</h2>
+          <div class="flex w-[100%]">
+              <div class="flex flex-wrap justify-evenly gap-10 w-[100%] pr-10 pl-10">
+
+                    <div class="flex flex-col w-[25%] p-5 mt-5 rounded-lg hover-rotate bg-black py-10"> 
+                        <div class="flex justify-center items-center">
+                          <img src="/images/DevLogo.png" alt="" className='w-[100px] h-[100px]'/>
                         </div>
-                        <div class="flex flex-col w-[80%]">
-                            <div class="font-bold text-gray-400  text-[25px]"><span>BACK END</span></div>
-                            <p className='text-white text-[18px]'> I excel in backend development. With a solid grasp of 
-                                server-side technologies and databases, I'm skilled at crafting efficient and reliable 
-                                software systems. Let's create exceptional digital experiences together!
-                            </p>
-                        </div>
+                        <div class="flex  justify-center font-bold text-gray-400 text-center  text-[25px] mt-10">BACK END</div>
+               
                     </div>
-                    <div class="flex  bg-backgray p-5 mt-5  hover-rotate"> 
+
+                    <div class="flex flex-col w-[25%] p-5 mt-5 rounded-lg hover-rotate  bg-black py-10"> 
                         
-                        <div class="flex justify-center items-center w-[25%] h-[100%]">
-                          <img src="/images/DesignLogo.png" alt="" className='w-[50%] h-[50%]'/>
+                        <div class="flex justify-center items-center">
+                          <img src="/images/DesignLogo.png" alt="" className='w-[100px] h-[100px]'/>
                         </div>
-                        <div class="flex flex-col w-[80%]">
-                            <div class="font-bold text-gray-400  text-[25px]"><span>FRONT END</span></div>
-                            <p className='text-white text-[18px]'> I'm a full stack developer with a passion for 
-                                    creating captivating user interfaces. Proficient in HTML, CSS, and JavaScript, 
-                                    I love turning design concepts into seamless and interactive web experiences. 
-                                    Let's build something beautiful and user-friendly! 
-                            </p>
+                        <div class="flex  justify-center font-bold text-gray-400 text-center  text-[25px] mt-10">FRONT END</div>
+                     
+                    </div>
+
+                    <div class="flex flex-col w-[25%] p-5 mt-5 rounded-lg hover-rotate  bg-black py-10"> 
+                        
+                        <div class="flex justify-center items-center">
+                          <img src="/images/DatabaseLogo.png" alt="" className='w-[100px] h-[100px]'/>
                         </div>
+                        <div class="flex  justify-center font-bold text-gray-400 text-center text-[25px] mt-10">DATABASE MANAGEMENT</div>
+               
+                    </div>
+                    <div class="flex flex-col w-[25%] p-5 mt-5 rounded-lg hover-rotate  bg-black py-10"> 
+                        
+                        <div class="flex justify-center items-center">
+                          <img src="/images/GraphicLogo.png" alt="" className='w-[100px] h-[100px]'/>
+                        </div>
+
+                        <div class="flex  justify-center font-bold text-gray-400 text-center  text-[25px] mt-10">GRAPHIC DESIGNING</div>
+                    
                     </div>
                     
-                    <div class="flex  bg-backgray p-5 mt-5  hover-rotate"> 
+                    <div class="flex flex-col w-[25%]  p-5 mt-5 rounded-lg hover-rotate  bg-black py-10"> 
                         
-                        <div class="flex justify-center items-center w-[25%] h-[100%]">
-                          <img src="/images/DatabaseLogo.png" alt="" className='w-[50%] h-[50%]'/>
+                        <div class="flex justify-center items-center">
+                          <img src="/images/API-LOGO.png" alt="" className='w-[100px] h-[100px]'/>
                         </div>
-                        <div class="flex flex-col w-[80%]">
-                            <div class="font-bold text-gray-400  text-[25px]"><span>DATABASE MANAGEMENT</span></div>
-                            <p className='text-white text-[18px]'> I specialize in database management. With expertise in designing, 
-                                    optimizing, and maintaining databases, I ensure seamless data organization 
-                                    and retrieval. Let's harness the power of data to drive smarter decisions 
-                                    and enhance your projects!
-                            </p>
-                        </div>
-                    </div>
-                    <div class="flex  bg-backgray p-5 mt-5  hover-rotate"> 
-                        
-                        <div class="flex justify-center items-center w-[25%] h-[100%]">
-                          <img src="/images/GraphicLogo.png" alt="" className='w-[50%] h-[50%]'/>
-                        </div>
-                        <div class="flex flex-col w-[80%]">
-                            <div class="font-bold text-gray-400  text-[25px]"><span>GRAPHIC DESIGNING</span></div>
-                            <p className='text-white text-[18px]'> a graphic designer with a flair for creativity and a strong command 
-                                    of Adobe Photoshop. I love transforming ideas into stunning visuals, 
-                                    whether it's designing logos, illustrations, or captivating graphics. 
-                                    Let's bring your vision to life through the magic of design! 
-                            </p>
-                        </div>
-                    </div>
-                    
-                    <div class="flex  bg-backgray p-5 mt-5  hover-rotate"> 
-                        
-                        <div class="flex justify-center items-center w-[25%] h-[100%]">
-                          <img src="/images/API-LOGO.png" alt="" className='w-[50%] h-[50%]'/>
-                        </div>
-                        <div class="flex flex-col w-[80%]">
-                            <div class="font-bold text-gray-400  text-[25px]"><span>API INTEGRATION</span></div>
-                            <p className='text-white text-[18px]'>I specialize in API integration. I have a knack for seamlessly connecting 
-                                    applications and services, enabling smooth data exchange and enhanced 
-                                    functionality. Whether it's integrating third-party APIs or building 
-                                    custom ones, I'm ready to streamline your digital ecosystem. Let's unlock 
-                                    the full potential of your projects together!
-                            </p>
-                        </div>
+                        <div class="flex  justify-center font-bold text-gray-400 text-center  text-[25px] mt-10">API INTEGRATION</div>
                     </div>
                     
 
@@ -94,12 +66,7 @@ export default function CanDo() {
 
           </div>
 
-          <div class="flex  justify-center items-center w-[40%]">
-              <div class=" flex justify-center items-center w-[80%]">
-                  <img src="/images/darkprofile.png" alt="" className='flex w-[100%] h-[80%] bg-black'/>
-              </div>
-          </div>
-         
+ 
 
         </div>
       </section>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,35 @@ import React from 'react'
 export default function Footer() {
   return (
     <>
-     <section>
-         <div id="Contact" className="flex justify-center w-[100%] h-[100vh] bg-darkgray">
-                
-
-
+     <section className='flex flex-col h-[700px] bg-black'>
+         <div id="Contact" className="flex justify-center w-[100%] h-[700px]">
+                <div className="flex justify-evenly w-[90%] h-[500px] mt-20"> 
+                  <div className="flex flex-col w-[50%]">
+                      <span className='text-[35px] text-red font-extrabold'>LETS TALK</span>
+                      <form action="" className='ml-10 mt-10'>
+                         <label for="input-label-with-helper-text" className="block text-sm font-medium mb-2 dark:text-white">Full Name</label>
+                         <input type="text" id="input-label-with-helper-text" className="w-[70%] h-10 rounded-md pl-3" placeholder="Enter Full Name" aria-describedby="hs-input-helper-text" />
+                         
+                         <label for="input-label-with-helper-text" className="block text-sm font-medium mb-2 dark:text-white mt-5">Email</label>
+                         <input type="email" id="input-label-with-helper-text" className="w-[70%] h-10 rounded-md pl-3" placeholder="you@site.com" aria-describedby="hs-input-helper-text" />
+                         
+                         <label for="input-label-with-helper-text" className="block text-sm font-medium dark:text-white  my-5">Message</label>
+                         <textarea
+                                id="message"
+                                name="Message"
+                                rows={3}
+                                className=" shadow-md p-2 pl-3 h-[150px] w-[70%] border-gray-300 bg-gray-100 rounded block border-0 py-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                defaultValue={''}
+                            />
+                      
+                          <button className='w-[70%] bg-red py-3 rounded-lg mt-5 font-bold'>SEND MESSAGE</button>
+                      </form>
+                  </div>
+                  <div className="flex w-[40%] "><span className='text-[35px] text-red font-extrabold'>FOLLOW ME ON</span></div>
+                </div>
          </div>
+         
+         <div className="flex w-[100%] h-[70px] items-center justify-center font-extrabold text-lg bg-gray-700 text-black">Copyright &copy;  2023 </div>
       </section>
     </>
   )
